9c59c93bf4d3f196fee77faedeb10fc8b8d45e62
Mohamad Hammash <mohamad.hammash@hotmail.com>
2022-07-22 15:38:42 +0200

    linkedin and github

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-
-function submitBtn(){
-   
-   const vcard = document.getElementById('v-card');
-   vcard.click();
+const app = {
+   btnAdd : document.getElementById('btn-add'),
+   btnLinkedIn: document.getElementById('btn-linked-in'),
+   btnGithub : document.getElementById('btn-github'),
+   avcard : document.getElementById('v-card'),
+   aLinkedIn : document.getElementById('a-linked-in'),
+   agithub: document.getElementById('a-github')
 };
+app.btnAdd.addEventListener('click',()=> submitBtn(app.avcard));
+app.btnLinkedIn.addEventListener('click',()=> submitBtn(app.aLinkedIn));
+app.btnGithub.addEventListener('click',()=> submitBtn(app.agithub));
 
-const btn = document.getElementById('btn-add');
-btn.addEventListener('click', submitBtn);
+
+function submitBtn(link){
+   
+  
+   link.click();
+};
